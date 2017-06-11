257b97ec096f2eaf4a9248b39de54e4f0ebf0958
Pedro Henrique Fornari <pedrohkf14@gmail.com>
2017-06-11 18:27:53 -0300

    scrap working with google news

--- googleScrap/spiders/urlgoogle.py
+++ googleScrap/spiders/urlgoogle.py
@@ -8,11 +8,20 @@ class UrlgoogleSpider(scrapy.Spider):
     name = "urlgoogle"
     #def __init__(self):
     allowed_domains = ["www.google.com.br"]
-    start_urls = ['http://www.google.com.br/search?q=jose+dirceu&tbm=nws&start=0']
+    start_urls = ['http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=0',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=10',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=20',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=30',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=40',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=50',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=60',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=70',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=80',
+                  'http://www.google.com.br/search?q=aecio+neves&tbm=nws&start=90']
 
     def parse(self, response):
         sel = Selector(response)
-        for link in (sel.xpath('//h3[@class="r _gJs"]/a[@class="l _PMs"]/@href').extract()):
+        for link in (sel.xpath("//h3[@class='r']/a/@href").extract()):
         #for link in sel.xpath('//h2[@class="esc-lead-article-title"]/a/@url').extract():
             item = GooglescrapItem()
             item['link'] = link

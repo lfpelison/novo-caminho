cd7cc4ac8e4d1855f40ef641b934b0b2d02eb296
Pedro Henrique Fornari <pedrohkf14@gmail.com>
2017-06-11 23:01:46 -0300

    removing pyc files and arranging scrapy files into two folders, one that runs the scrapper from a python script and the other runs from cmd line. Also added number of pages to scrap in the scrapScript

--- scrapScript/testSpider.py
+++ scrapScript/testSpider.py
@@ -1,9 +1,10 @@
 from scrapy.crawler import CrawlerProcess
 from GoogleUrls import UrlgoogleSpider
 searchWords = ('jose', 'maria')
-MySpider = UrlgoogleSpider()
+MySpider = UrlgoogleSpider(1)
+print MySpider.start_urls
 process = CrawlerProcess({
   'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
-process.crawl(MySpider,*searchWords)
+process.crawl(MySpider, 10, *searchWords)
 process.start() # the script will block here until all crawling jobs are finishe

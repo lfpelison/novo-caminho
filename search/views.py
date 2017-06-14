7f3ac36839fb689aaafc86c3b7f99cb8bef67fac
Henke <eduardo_henke@hotmail.com>
2017-06-13 22:01:47 -0300

    Add template

--- search/views.py
+++ search/views.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from django.http import HttpResponse
 from news_urls import news_urls
 
 def index(request):
     url_list = news_urls.get_urls( ("jose dirceu"), ('google'), 2)
-    url_list = ["<a href={0}>{0}</a>".format(url) for url in url_list]
-    return HttpResponse('<br><br>'.join(url_list))
+    template = loader.get_template('search/index.html')
+    context = {
+        'url_list': url_list,
+    }
+    return HttpResponse(template.render(context, request))

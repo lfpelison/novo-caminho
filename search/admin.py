87d3534c41775d6d07eed523964b36ca7bd002a9
Henke <eduardo_henke@hotmail.com>
2017-07-03 21:54:35 -0300

    Improve models, and change .gitignore(migrations must be commited)

--- search/admin.py
+++ search/admin.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Query, EntityQuery
+from .models import Query, Entity
 
 admin.site.register(Query)
-admin.site.register(EntityQuery)
+admin.site.register(Entity)
 
 # Register your models here.
